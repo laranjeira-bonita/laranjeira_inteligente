c66f7fe42296b8faa761bf0e44b7ad29b07bea4c
shu qi <shu@fetch.ly>
2025-06-30 22:12:53 -0300

    basc login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:email]
   
   has_many :purchases, dependent: :destroy
   has_many :tickers, dependent: :destroy
