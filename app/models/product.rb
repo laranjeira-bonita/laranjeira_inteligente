eaa96c299a45f41d95d452ceeb648f4f640743cb
shu qi <shu@fetch.ly>
2025-06-30 22:12:25 -0300

    add product_promotion enum

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
     belongs_to :store
     belongs_to :promotion, optional: true
     has_many :descriptions, as: :describable, dependent: :destroy
+    enum category: { gift_card: 0, electronics: 1 }
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -3,4 +3,5 @@ class Promotion < ApplicationRecord
     belongs_to :store
     has_many :descriptions, as: :describable, dependent: :destroy
     has_many :tickers, dependent: :destroy
+    enum off_type: { gift_card: 0, disccount: 1 }
 end
